30320b3ed0bd4ae94595303e2d51a9e86c866e79
itz-Hiru <hirumithagithub@gmail.com>
2025-04-21 09:45:24 +0530

    Created website structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import "./App.css";
+
+import Footer from "./components/Footer/Footer.component";
+import Header from "./components/Header/Header.component";
+
+import About from "./sections/About/About.section";
+import Features from "./sections/Features/Features.section";
+import Home from "./sections/Home/Home.section";
+import Menu from "./sections/Menu/Menu.section";
+import Reservation from "./sections/Reservation/Reservation.section";
+import Testimonials from "./sections/Testimonials/Testimonials.section";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Header />
+      <Home />
+      <Reservation />
+      <Features />
+      <About />
+      <Menu />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/sections/Reservation/Reservation.section.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./reservation.css"
+
+const Reservation = () => {
+  return (
+    <div>Reservation</div>
+  )
+}
+
+export default Reservation
